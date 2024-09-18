aa10436eba10133d06841bb45bb0cb122736c60c
thekillerssss <164350352+thekillerssss@users.noreply.github.com>
2024-09-18 13:22:56 +0200

    Update QuestionMultipleChoices.java

--- quiz_app/src/main/files/QuestionMultipleChoices.java
+++ quiz_app/src/main/files/QuestionMultipleChoices.java
@@ -1,19 +1,49 @@
-public class QuestionMultipleChoices implements Question{
-    private int questionId;
-    private String questionText;
-    //Save answers in an array instead?
-    private String correctAnswer;
-    private String falseAnswer1;
-    private String falseAnswer2;
+import java.util.ArrayList;
 
+public class QuestionMultipleChoices implements Question{
+
+   //question identifier
+    private final int questionId;
+    //The question description
+    private final String questionText;
+    //An array of strings, containing the three alternative answers.
+    private final ArrayList <String> answers = new ArrayList<>();
+    //The only correct answer to the question
+    private final String correctAnswer;
+    // The aha moment!
+    private final String trivia;
+
+
+    public QuestionMultipleChoices(String questionText,String correctAnswer, String falseAnswer1,String falseAnswer2,String trivia,int questionId){
+       this.questionText = questionText;
+        this.correctAnswer = correctAnswer;
+        answers.add(correctAnswer);
+        answers.add(falseAnswer1);
+        answers.add(falseAnswer2);
+        this.trivia = trivia;
+        this.questionId = questionId;
+
+    }
+
+    @Override
     public int getQuestionId(){
         return this.questionId;
     }
-    
+    @Override
     public String getQuestion(){
         return this.questionText;
     }
-    public String getAnswer(){
+    @Override
+    public ArrayList<String> getAnswers(){
+        return this.answers;
+    }
+
+    public String getTrivia(){
+        return this.trivia;
+    }
+
+    public String getCorrectAnswer(){
         return this.correctAnswer;
     }
+
 }
