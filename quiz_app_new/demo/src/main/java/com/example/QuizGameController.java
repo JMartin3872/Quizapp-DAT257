9768a3da0aefb98607ecb4c0db8da5afe09eb038
GorillaDev420 <164350417+GorillaDev420@users.noreply.github.com>
2024-09-24 23:06:37 +0200

    Update QuizGameController.java
    Changed first Window popup to Login instead of Main menu

--- quiz_app_new/demo/src/main/java/com/example/QuizGameController.java
+++ quiz_app_new/demo/src/main/java/com/example/QuizGameController.java
@@ -1,14 +1,30 @@
 package com.example;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
 public class QuizGameController {
 
     private QuizGameModel model;
+    private LoginView loginView;
+    private QuizGameView quizGameView;
 
     public QuizGameController(){
-        new QuizGameView(model);
+        this.loginView =new LoginView(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                if(loginView.getUsername() != null){
+                    loginView.setVisible(false);
+                    model.setUsername(loginView.getUsername());
+                    quizGameView = new QuizGameView(model);
+                    
+                }
+                
+            }
+        });
+        
     }
 
     public static void main(String[] args){
