1b936600b0a2c4cb13e3c58898f62e73a0d78523
Martin <martbern@chalmers.se>
2024-09-30 13:02:44 +0200

    Placeholder classes for true/false question and model.

---
+++ quiz_app_new/demo/src/main/java/com/example/QuestionTrueFalse.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class QuestionTrueFalse implements Question{
+    public int getQuestionId(){
+        return 0;
+    }
+    public String getQuestionText(){
+        return "";
+    }
+
+    public ArrayList<String> getAnswers(){
+        return null;
+    }
+    public void shuffleAnswers(){
+
+    }
+    public String getTrivia(){
+        return "";
+
+    }
+    public String getCorrectAnswer(){
+        return "";
+
+    }
+}
