1c631e1082eb2c10a22ccdd32d6a9c67fe8bd4eb
Martin <martbern@chalmers.se>
2024-10-02 13:53:37 +0200

    Model, questiontype and reader for true/false quiz

--- quiz_app_new/demo/src/main/java/com/example/ModelTrueFalse.java
+++ quiz_app_new/demo/src/main/java/com/example/ModelTrueFalse.java
@@ -1,5 +1,109 @@
 package com.example;
+import java.util.*;
 
 public class ModelTrueFalse {
+    private Map<Integer, Question> questionMap = new HashMap<>();
+    private int currentQuestionId = 0; // Default to the first question
+    private String userName;
+    private int score = 0;
+    private int correctAnswers = 0;
+    private int wrongAnswers = 0;
 
+    private String trueOrFalseInfo = "This quiz presents you with true or false questions.\n\n"
+        + "You are tasked with selecting the correct answer.\n\n"
+        + "Select your answer and press \"Submit Answer\".";
+
+
+    public ModelTrueFalse() {
+        // QuestionReaderMultipleChoices used to read questions from  testQeustions.txt
+        QuestionReaderTrueFalse reader = new QuestionReaderTrueFalse();
+        LinkedList<Question> questions = reader.readQuestionFile("True-False-Questions.txt");
+
+        for (Question question : questions) {
+            questionMap.put(question.getQuestionId(), question);
+        }
+    }
+
+    // Fetch the current question
+    public Question getCurrentQuestion() {
+        return questionMap.get(currentQuestionId);
+    }
+
+    // Get current question ID
+    public int getCurrentQuestionId() {
+        return getCurrentQuestion().getQuestionId();
+    }
+
+    // Get current question text
+    public String getCurrentQuestionText() {
+        return getCurrentQuestion().getQuestionText();
+    }
+
+    // Get the answer alternatives of the current question
+    public ArrayList<String> getCurrentQuestionAnswers() {
+        return getCurrentQuestion().getAnswers();
+    }
+
+    // Fetch trivia for the current question
+    public String getCurrentQuestionTrivia() {
+        return questionMap.get(currentQuestionId).getTrivia();
+    }
+
+    // Get the correct answer for the current question
+    public String getCurrentQuestionCorrectAnswer() {
+        return questionMap.get(currentQuestionId).getCorrectAnswer();
+    }
+
+    public boolean checkAnswer(String userAnswer) {
+        return getCurrentQuestionCorrectAnswer().equals(userAnswer);
+    }
+
+    // Proceed to the next question
+    public void nextQuestion() {
+        if (questionMap.containsKey(currentQuestionId + 1)) {
+            currentQuestionId++;
+        }
+    }
+
+    // Returns the total amount of questions in the quiz
+    public int getTotalQuestions() {
+        return questionMap.size();
+    }
+
+    // Checks whether the current quiz is finished
+    public boolean isFinished() {
+        return currentQuestionId >= questionMap.size() - 1;
+    }
+
+    // Gets info about True or False quiz
+    public String getTFQuizInfo() {
+        return trueOrFalseInfo;
+    }
+
+    // Sets the username
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    // For now, these functions reside in the model. Will be moved to User class later
+    public void correctAnswer() {
+        this.correctAnswers++;
+        score++;
+    }
+
+    public void wrongAnswer() {
+        this.wrongAnswers++;
+    }
+
+    public int getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public int getWrongAnswers() {
+        return wrongAnswers;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
--- quiz_app_new/demo/src/main/java/com/example/QuestionTrueFalse.java
+++ quiz_app_new/demo/src/main/java/com/example/QuestionTrueFalse.java
@@ -1,27 +1,49 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class QuestionTrueFalse implements Question{
+    //question identifier
+    private final int questionId;
+    //The question description
+    private final String questionText;
+    //An array of strings, containing the three alternative answers.
+    private final ArrayList <String> answers = new ArrayList<String>();
+    //The only correct answer to the question
+    private final String correctAnswer;
+    // The aha moment!
+    private final String trivia;
+
+    public QuestionTrueFalse(String questionText,String correctAnswer, String wrongAnswer, String trivia, int questionId){
+        this.questionText = questionText;
+        this.correctAnswer = correctAnswer;
+        answers.add(correctAnswer);
+        answers.add(wrongAnswer);
+        this.trivia = trivia;
+        this.questionId = questionId;
+        Collections.shuffle(answers); // added to shuffle the order of the answers on the buttons in MultipleChoicesQuizView!
+    }
+
     public int getQuestionId(){
-        return 0;
+        return this.questionId;
     }
     public String getQuestionText(){
-        return "";
+        return this.questionText;
     }
 
     public ArrayList<String> getAnswers(){
-        return null;
+        return this.answers;
     }
     public void shuffleAnswers(){
-
+        
     }
     public String getTrivia(){
-        return "";
+        return this.trivia;
 
     }
     public String getCorrectAnswer(){
-        return "";
+        return this.correctAnswer;
 
     }
 }
