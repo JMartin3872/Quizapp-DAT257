fdfbc2e3aee7736738b2c64bd11d519041416ec6
Reza Firuzian <firuzian@chalmers.se>
2024-10-15 12:59:21 +0200

    Added 2 unit tests.

---
+++ quiz_app_new/demo/src/test/java/com/example/ModelTrueFalseTest.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class ModelTrueFalseTest {
+    @Test
+    public void testGetScore() {
+        ModelTrueFalse modelTrueFalse = new ModelTrueFalse();
+        int intialScore = modelTrueFalse.getScore();
+        Assertions.assertEquals(0, intialScore);        
+    }
+
+    @Test
+    public void testGetScoreAfterCorrectIncrease() {
+        ModelTrueFalse modelTrueFalse = new ModelTrueFalse();
+        modelTrueFalse.correctAnswer();
+        int afterScore = modelTrueFalse.getScore();
+        Assertions.assertEquals(1, afterScore);      
+    }
+}
